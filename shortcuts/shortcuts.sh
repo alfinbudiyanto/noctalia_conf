7918328ae2558149965e04361568b236c1d4e2d1
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2026-08-20 22:41:58 +0700

    add shortcuts program so shortcuts of noctalia popup in the correct monitor

--- shortcuts/shortcuts.sh
+++ shortcuts/shortcuts.sh
@@ -1,24 +1,26 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+panel_id="${1:-panel-toggle launcher}"
+
 output=$(driftwm msg state | grep '^outputs' | awk '{print $2}')
 
 [[ "$output" != 1 ]] || {
-    noctalia msg panel-toggle launcher
+    noctalia msg "$panel_id"
     exit 0
 }
 
 panel=$(noctalia msg status | grep '"activePanelId"' | awk '{print $2}' | tr -d ',"')
 
 [[ "$panel" == null ]] || {
-    noctalia msg panel-toggle launcher
+    noctalia msg "$panel_id"
     exit 0
 }
 
 fuzzel &
 fuzzel_pid=$!
 
-noctalia msg panel-toggle launcher
+noctalia msg "$panel_id"
 
 kill "$fuzzel_pid" 2>/dev/null || true
 
