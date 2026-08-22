fce9dac06147a171b82709615640d801d66e2ab1
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2026-08-22 18:26:04 +0700

    clean code for shortcuts

--- shortcuts/shortcuts.sh
+++ shortcuts/shortcuts.sh
@@ -2,7 +2,6 @@
 set -euo pipefail
 
 panel_id="${1:-panel-toggle launcher}"
-
 output=$(driftwm msg state | grep '^outputs' | awk '{print $2}')
 
 [[ "$output" != 1 ]] || {
@@ -24,4 +23,3 @@ noctalia msg "$panel_id"
 
 kill "$fuzzel_pid" 2>/dev/null || true
 
-exit 0
