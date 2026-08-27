5bb51579b74242b99a78fe354114ce65c881be50
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2026-08-27 19:22:30 +0700

    finish hook program when session locked

--- hooks/session-locked.sh
+++ hooks/session-locked.sh
@@ -7,9 +7,14 @@ wifi_status=$(noctalia msg wifi-status)
 bt_status=$(noctalia msg bluetooth-status)
 
 state_dir="${XDG_STATE_HOME:-$HOME/.local/state}/noctalia"
-#wifi_state=$(<"$state_dir/wifi-state")
+
+wifi_state="$state_dir/wifi-state"
 bt_state="$state_dir/bt-state"
 
-[[ "$bt_status" != $(<"$bt_state") ]] || exit 0
+if [[ $(<"$wifi_state") != "$wifi_status" ]]; then
+    printf '%s\n' "$wifi_status" > "$wifi_state"
+fi
 
-printf '%s\n' "$bt_status" > "$bt_state"
+if [[ $(<"$bt_state") != "$bt_status" ]]; then
+    printf '%s\n' "$bt_status" > "$bt_state"
+fi
