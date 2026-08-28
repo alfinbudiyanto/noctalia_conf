dec4f8defd775cf210d86a34e14bffa8be4e8691
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2026-08-28 23:29:27 +0700

    finish hooks when session locked & unlocked, session-locked & session unlocked

--- hooks/session-locked.sh
+++ hooks/session-locked.sh
@@ -2,19 +2,20 @@
 set -euo pipefail
 
 playerctl pause || true
+noctalia msg bar-hide
 
-wifi_status=$(noctalia msg wifi-status)
 bt_status=$(noctalia msg bluetooth-status)
+wifi_status=$(noctalia msg wifi-status)
 
 state_dir="${XDG_STATE_HOME:-$HOME/.local/state}/noctalia"
 
-wifi_state="$state_dir/wifi-state"
 bt_state="$state_dir/bt-state"
-
-if [[ $(<"$wifi_state") != "$wifi_status" ]]; then
-    printf '%s\n' "$wifi_status" > "$wifi_state"
-fi
+wifi_state="$state_dir/wifi-state"
 
 if [[ $(<"$bt_state") != "$bt_status" ]]; then
     printf '%s\n' "$bt_status" > "$bt_state"
+fi
+
+if [[ $(<"$wifi_state") != "$wifi_status" ]]; then
+    printf '%s\n' "$wifi_status" > "$wifi_state"
 fi
--- hooks/session-unlocked.sh
+++ hooks/session-unlocked.sh
@@ -2,15 +2,32 @@
 set -euo pipefail
 
 noctalia msg bar-show
+noctalia msg dpms-on && notify-send 'Session resumed'
 
 bt_status=$(noctalia msg bluetooth-status)
-state_dir=$(<"${XDG_STATE_HOME:-$HOME/.local/state}/noctalia/bt-state")
+wifi_status=$(noctalia msg wifi-status)
 
-[[ "$bt_status" != "$state_dir" ]] || exit 0
+state_dir="${XDG_STATE_HOME:-$HOME/.local/state}/noctalia"
 
-[[ "$bt_status" == "on" ]] || {
-    noctalia msg bluetooth-enable
-    exit 0
-}
+bt_state_file=$(<"$state_dir/bt-state")
+wifi_state_file=$(<"$state_dir/wifi-state")
 
-noctalia msg bluetooth-disable
+if [[ "$bt_status" != "$bt_state_file" ]]; then
+    if [[ "$bt_status" == "on" ]]; then
+        noctalia msg bluetooth-enable
+    fi
+
+    if [[ "$bt_status" == "off" ]]; then
+        noctalia msg bluetooth-disable
+    fi
+fi
+
+if [[ "$wifi_status" != "$wifi_state_file" ]]; then
+    if [[ "$wifi_status" == "on" ]]; then
+      	noctalia msg wifi-enable
+    fi
+
+    if [[ "$wifi_status" == "off" ]]; then
+        noctalia msg wifi-disable
+    fi
+fi
