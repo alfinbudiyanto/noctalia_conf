bf37a001f46675421307f11ad9756725fddc369c
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2026-08-28 20:33:16 +0700

    fixed bug for hook when startup bluetooth aint work properly, startup.sh

--- hooks/startup.sh
+++ hooks/startup.sh
@@ -3,18 +3,18 @@ set -euo pipefail
 
 systemctl --user start noctalia-ready.target || true
 
-wifi_status=$(noctalia msg wifi-status)
 bt_status=$(noctalia msg bluetooth-status)
+wifi_status=$(noctalia msg wifi-status)
 
 state_dir="${XDG_STATE_HOME:-$HOME/.local/state}/noctalia"
 
-wifi_state=$(<"$state_dir/wifi-state")
 bt_state=$(<"$state_dir/bt-state")
+wifi_state=$(<"$state_dir/wifi-state")
+
+if [[ "$bt_status" != "$bt_state" ]]; then
+    noctalia msg bluetooth-toggle 
+fi
 
 if [[ "$wifi_status" != "$wifi_state" ]]; then
     noctalia msg wifi-toggle
-fi
-
-if [[ "$bt_status" != "$bt_state" ]]; then
-    noctalia msg bluetooth-toggle
 fi
