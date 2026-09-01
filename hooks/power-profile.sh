973a466d71c904442b78eded95fb578f4d500842
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2026-09-01 22:42:43 +0700

    add protection for noctalia variable and clean code. hooks/power-profile.sh

--- hooks/power-profile.sh
+++ hooks/power-profile.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-case "$NOCTALIA_POWER_PROFILE" in
+case "${NOCTALIA_POWER_PROFILE:-}" in
     power-saver)
         noctalia msg brightness-set eDP-1 3
         ;;
-    balanced)
-        noctalia msg brightness-set eDP-1 10
-        ;;
-    performance)
+    balanced|performance)
         noctalia msg brightness-set eDP-1 10
         ;;
 esac
