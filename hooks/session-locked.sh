9f7e08215256b944e31c355beb14b35934d90b58
alfinbudiyanto <alfinbudiyantogit@gmail.com>
2026-09-01 21:25:25 +0700

    fixing program when booting and sleep(locked), file session-locked & unlocked, startup

--- hooks/session-locked.sh
+++ hooks/session-locked.sh
@@ -9,13 +9,13 @@ wifi_status=$(noctalia msg wifi-status)
 
 state_dir="${XDG_STATE_HOME:-$HOME/.local/state}/noctalia"
 
-bt_state="$state_dir/bt-state"
-wifi_state="$state_dir/wifi-state"
+bt_state_file=$(<"$state_dir/bt-state")
+wifi_state_file=$(<"$state_dir/wifi-state")
 
-if [[ $(<"$bt_state") != "$bt_status" ]]; then
+if [[ "$bt_state_file" != "$bt_status" ]]; then
     printf '%s\n' "$bt_status" > "$bt_state"
 fi
 
-if [[ $(<"$wifi_state") != "$wifi_status" ]]; then
+if [[ "$wifi_state_file" != "$wifi_status" ]]; then
     printf '%s\n' "$wifi_status" > "$wifi_state"
 fi
--- hooks/startup.sh
+++ hooks/startup.sh
@@ -3,18 +3,15 @@ set -euo pipefail
 
 systemctl --user start noctalia-ready.target || true
 
-bt_status=$(noctalia msg bluetooth-status)
-wifi_status=$(noctalia msg wifi-status)
-
 state_dir="${XDG_STATE_HOME:-$HOME/.local/state}/noctalia"
 
-bt_state=$(<"$state_dir/bt-state")
-wifi_state=$(<"$state_dir/wifi-state")
+bt_state_file=$(<"$state_dir/bt-state")
+wifi_state_file=$(<"$state_dir/wifi-state")
 
-if [[ "$bt_status" != "$bt_state" ]]; then
-    noctalia msg bluetooth-toggle 
+if [[ "$bt_state_file" == "off" ]]; then
+    noctalia msg bluetooth-disable
 fi
 
-if [[ "$wifi_status" != "$wifi_state" ]]; then
-    noctalia msg wifi-toggle
+if [[ "$wifi_state_file" == "off" ]]; then
+    noctalia msg wifi-disable
 fi
